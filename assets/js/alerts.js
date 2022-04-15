2fc552d46521a22fc5a745e4263c8e4649b8ed3e
ssandorov <stefansandorovssb@gmail.com>
2022-04-15 17:39:58 +0200

    comentarios sobre las excepciones del obj window

--- assets/js/alerts.js
+++ assets/js/alerts.js
@@ -5,7 +5,9 @@ prompt("¿Cuál es tú nombre?", "Pepito Grillo");
 confirm("¿Estás seguro de borrar esto?");
 
 /*
-Estas tres funciones forman parte del objeto window, por lo que no funcionarán fuera del navegador web
+Estas tres funciones forman parte del objeto window, por lo que no funcionarán fuera del navegador web, esto quiere
+decir que no se pueden utilizar tampoco en el backend. Básicamente las excepciones se encuentran con el objeto window,
+ya que este opera en el navegador pero no en el servidor o en la terminal
 Lo más cercano es el objeto global de node
 Hoy en día ya hay librerías que ofrecen mejores soluciones a estas tres funciones
 */
