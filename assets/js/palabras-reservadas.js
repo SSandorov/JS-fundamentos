0b6a6ab16435e143dbda388debb4159bf4b84207
ssandorov <stefansandorovssb@gmail.com>
2022-04-18 20:12:57 +0200

    uso de palabras reservadas

---
+++ assets/js/palabras-reservadas.js
@@ -0,0 +1,19 @@
+// el uso de las pabras reservadas no es posible
+// let const = 123;
+
+// El enlace del validador de nombre de variables es perfecto para saber si la variable que vamos a emplear es
+// válida o no
+// También tenemos el archivo descargado que nos muestra cómo podemos crear variables
+
+// Estándar de nombres variables
+/*
+    1. No puede empezar por un número
+    2. Si puede empezar con un guion bajo
+    3. Si puede utilizar el símbolo del $
+    4. No puede llevar un punto
+    5. Utilizar camel case para nombrar variables
+    6. Utilizar open camel case para nombrar clases
+    7. No emplear caracteres especiales
+    8. Utilizar el guion bajo para separar números en lugar de putnos y comas
+
+*/
